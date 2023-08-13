e131f7975b18ea1e6bb7036275634502a7e467cc
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2023-08-13 13:06:42 +0600

    add module-25

---
+++ md-25/script.js
@@ -0,0 +1,41 @@
+//add comment::::
+
+// document.getElementById("submit").addEventListener("click", function () {
+//   const comments = document.getElementById("comments");
+//   const textArea = document.getElementById("textArea");
+//   const textAreaValue = textArea.value;
+//   console.log(textAreaValue);
+//   const newParagraph = document.createElement("p");
+//   newParagraph.innerText = textAreaValue;
+//   comments.appendChild(newParagraph);
+//   textArea.value = "";
+// });
+
+
+/// conditional delete like github::::
+// const passInfo = document.getElementById('pass_info');
+// const inputField = document.getElementById('input_field');
+
+
+// document.getElementById('input_field').addEventListener('keyup', (event)=>{
+//     const deleteBtn = document.getElementById('delete_btn');
+//     const inputValue = event.target.value;
+//     console.log(inputValue);
+//     if(inputValue === "delete"){
+//         deleteBtn.removeAttribute("disabled")
+//     }
+//     else{
+//         deleteBtn.setAttribute("disabled", true)
+//     }
+// })
+
+
+// document.getElementById('delete_btn').addEventListener('click', () =>{
+//     // console.log('clicked');
+//     passInfo.style.display = "none";
+//     inputField.value = "";
+// })
+
+//propagation::
+
+
